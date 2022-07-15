02dbbaa4354fe69a7aa04b105281135525e50986
zhiwei-w-luo <zhiwei.w.luo@gmail.com>
2022-07-15 14:45:07 +0800

    fix nft contract

--- contracts/roisupe/roisupe_nft.sol
+++ contracts/roisupe/roisupe_nft.sol
@@ -18,7 +18,7 @@ contract Roisupe is ERC721, Ownable {
   
   uint256 public cost = 0.01 ether;
   uint256 public maxSupply = 10000;
-  uint256 public maxMintAmountPerTx = 5;
+  uint256 public maxMintAmountPerTx = 1;
   uint256 public faucet = 0.01 ether;
 
 
