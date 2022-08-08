872964d89aafbbd259ea85e93c1acbdd6d783562
zhiwei-w-luo <zhiwei.w.luo@gmail.com>
2022-08-08 12:51:39 +0800

    add bootnode

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -23,7 +23,7 @@ import "github.com/ethereum/go-ethereum/common"
 var MainnetBootnodes = []string{
 	// Roisupe mainnet Bootnodes
 	"enode://2031d85529cf917b7584c5e45b857c87ab4c6916458202803b5927dde5e915e43be71d3334e2eae725e99dcf2d2ac514392ac3fd337cd918da5d57b062a0734c@146.190.36.237:30303",   // bootnode-aws-ap-southeast-1-001
-	"enode://e8e3ef7a7d270cab8228ad32986e81deece8662f375a1f369caad4f86f418b248bd86903ce2d464a7e9aed087aead24a5cb447dffac0c31b3ce50e6ebaacbd29@143.198.200.178:30303",     // bootnode-aws-us-east-1-001
+	"enode://47bfa78d554a5d58fe0c600e9fdaf585d968a85578625889013d1a1a9476db44fe4e71a58eb3b218ed1ffb500735b655c60703a3657d0aaadb5f16fb259eda21@143.198.200.178:30303",     // bootnode-aws-us-east-1-001
 	"enode://ee37720d3fe79c6fa153a893ba53f4550cefe6a7b25b2c5a389c77e673c588b6bb4b79b8596f34716db705ad0bfe5117418cc385a489eac8b5dd8146b7dcf917@127.0.0.1:30305",     // bootnode-aws-us-east-1-001
 	"enode://cfa84b723e63a954db4b2b91b03556ca58264a33f7646020b5c1fff974c7057d5d8f6f7accfdadfff611cf39a0d72110baaa31749c8ebb0e9dd7ed3fc6f3a8a3@127.0.0.1:30306",     // bootnode-aws-us-east-1-001
 	"enode://96c3b589a2d2a052c481586fdb509ca80baa60cddcd37f82f2b193fe272907681f1feadad18436b07c322f3488a13bc1c7b0a44d1e7fb7cfdebe780ea7a7a150@127.0.0.1:30307",     // bootnode-aws-us-east-1-001
