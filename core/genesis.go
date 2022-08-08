74fdaa3ae91f42b4e6b0567ac5f7855fae60a664
zhiwei-w-luo <zhiwei.w.luo@gmail.com>
2022-08-08 12:36:12 +0800

    update timestamp

--- core/genesis.go
+++ core/genesis.go
@@ -363,7 +363,7 @@ func DefaultGenesisBlock() *Genesis {
 	var extra = []byte("Roisupe")
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
-		Timestamp:  1657783767,
+		Timestamp:  1659933348,
 		ExtraData:  append(extra, make([]byte, 32-len(extra))...),
 		GasLimit:   params.GenesisGasLimit,
 		Difficulty: big.NewInt(1),
